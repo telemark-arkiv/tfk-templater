ec7dd426d5fc5f5ad8a1ed31c55df84fbf7adeeb
Geir Gåsodden <geir.gasodden@pythonia.no>
2015-05-13 22:49:47 +0200

    Changes code style

--- index.js
+++ index.js
@@ -1,84 +1,99 @@
-'use strict'
+'use strict';
 
-var fs = require('fs')
-var Docxtemplater = require('docxtemplater')
-var async = require('async')
+var fs = require('fs');
+var Docxtemplater = require('docxtemplater');
+var async = require('async');
 
-var options, content, doc, buf
+var options;
+var content;
+var doc;
+var buf;
 
 // Function: Check if all options are set
 function checkOpts(callback) {
   if (!options) {
-    return callback('Missing required input: options', null)
+    return callback('Missing required input: options', null);
   }
 
   if (!options.inputfile) {
-    return callback('Missing required input: options.inputfile', null)
+    return callback('Missing required input: options.inputfile', null);
   }
 
   if (!options.outputfile) {
-    return callback('Missing required input: options.outputfile', null)
+    return callback('Missing required input: options.outputfile', null);
   }
 
   if (!options.data) {
-    return callback('Missing required input: options.data', null)
+    return callback('Missing required input: options.data', null);
   }
-  return callback()
+  return callback();
 }
 
 // Function: Read docx file
 function readFile(callback) {
-  content = fs.readFileSync(options.inputfile,'binary', function(err, res) {
-    if (err) return callback(err, null)
-  })
-  return callback()
+  content = fs.readFileSync(options.inputfile, 'binary', function(err, res) {
+    if (err) {
+      return callback(err, null);
+    }
+  });
+  return callback();
 }
 
 // Function: Load Template
 function templater(callback) {
   doc = new Docxtemplater(content,  function(err, res) {
-    if (err) return callback(err, null)
-  })
-  return callback()
+    if (err) {
+      return callback(err, null);
+    }
+  });
+  return callback();
 }
 
 // Function: fills in data in template
 function setDocData(callback) {
   doc.setData(options.data, function(err) {
-    if (err) return callback(err, null)
-  })
-  return callback()
+    if (err) {
+      return callback(err, null);
+    }
+  });
+  return callback();
 }
 
 // Function: Render document
 function renderDoc(callback) {
   doc.render(function(err) {
-    if (err) return callback(err, null)
-  })
-  return callback()
+    if (err) {
+      return callback(err, null);
+    }
+  });
+  return callback();
 }
 
 // Function: Generate doc buffer
 function generateBuffer(callback) {
   buf = doc.getZip().generate({type:'nodebuffer'}, function(err, buf) {
-    if (err) return callback(err, null)
-  })
-  return callback()
+    if (err) {
+      return callback(err, null);
+    }
+  });
+  return callback();
 }
 
 // Function: Write finished document
 function writeFile(callback) {
-  fs.writeFileSync(options.outputfile, buf, 'utf-8', function (err) {
-    if (err) return callback(err, null)
-  })
-  return callback('Wrote file ' + options.outputfile)
+  fs.writeFileSync(options.outputfile, buf, 'utf-8', function(err) {
+    if (err) {
+      return callback(err, null);
+    }
+  });
+  return callback('Wrote file ' + options.outputfile);
 }
 
 // Constructor: Do all the things
 function create(opts, callback) {
 
   opts.files.forEach(function(arr) {
-    options = arr
+    options = arr;
     async.series([
       checkOpts,
       readFile,
@@ -87,12 +102,12 @@ function create(opts, callback) {
       renderDoc,
       generateBuffer,
       writeFile
-    ], function (err, res) {
+    ], function(err, res) {
       if (err) {
-        console.error(err)
+        console.error(err);
       }
-    })
-  })
+    });
+  });
 }
 
 // Export the class
